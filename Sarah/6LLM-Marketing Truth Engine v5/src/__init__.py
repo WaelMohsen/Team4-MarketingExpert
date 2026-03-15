c1ca783a2d0375093974659d3e2a0dde07a7d9ff
Mu-Magdy <muhammedmagdy1015@gmail.com>
2026-03-15 09:18:13 +0200

    chore: remove legacy Sarah directory

--- Sarah/6LLM-Marketing Truth Engine v5/src/__init__.py
+++
@@ -1,7 +0,0 @@
-from .data_exploration   import *       # Import everything from the data_exploration subpackage
-from .data_preprocessing import *       # Import everything from the data_preprocessing subpackage
-from .feature_extraction import *       # Import everything from the feature_extraction subpackage
-from .modeling           import *       # Import everything from the modeling subpackage
-from .evaluation         import *
-from .utils              import *       # Import everything from the utils subpackage
-from .LLMs_and_Prompts   import *
